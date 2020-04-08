ccef51dd2e1bdac0a059bd1dbd1dfd1d32537c7e
Michel Alexander <michel@freshworks.io>
2020-04-08 08:33:39 -0700

    on admin action, update DB accordingly via PATCH from form/:id

--- server/server.js
+++ server/server.js
@@ -57,16 +57,19 @@ app.patch(`${apiBaseUrl}/form/:id`,
   passport.authenticate('jwt', { session: false }),
   async (req, res) => {
     const { id } = req.params;
-    const item = {
+    const params = {
       TableName: 'forms',
-      Item: {
-        ...req.body,
-        id,
+      Key: { id },
+      UpdateExpression: 'set notes = :n, determination = :d',
+      ExpressionAttributeValues: {
+        ':n': req.body.notes,
+        ':d': req.body.determination,
       },
       ConditionExpression: 'attribute_exists(id)',
+      ReturnValues: 'UPDATED_NEW',
     };
     try {
-      await db.put(item).promise();
+      await db.update(params).promise();
       res.json({ id });
     } catch (error) {
       res.status(500).json({ error: 'Failed to update submission' });
@@ -84,6 +87,7 @@ app.get(`${apiBaseUrl}/form/:id`,
     };
     try {
       const item = await db.get(params).promise();
+      console.dir(item)
       if (Object.keys(item).length === 0) return res.status(404).json({ error: `No submission with ID ${id}` });
       return res.json(item.Item);
     } catch (error) {
