5ea162017ab0f3ab861e44550965a1192e7655d8
Ashton Meuser <ashtonmeuser@gmail.com>
2020-04-09 09:02:48 -0700

    Fix syntax errors

--- server/pdf.js
+++ server/pdf.js
@@ -8,25 +8,20 @@ const frontendUrl = process.env.FE_URL || 'http://localhost:80';
 // Uses JWT access token in URL
 // FE fetches data from API using JWT
 const createPdf = async (id, accessToken) => {
-  try {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto(`${frontendUrl}/renderpdf/${id}/${accessToken}`, {
-      waitUntil: 'networkidle2',
-    });
-    await page.setViewport({ width: 1680, height: 1050 });
-    if (!fs.existsSync('./pdfs')) fs.mkdirSync('./pdfs');
-    const pdfPath = path.join(__dirname, 'pdfs', `travellerScreeningReport-${id}.pdf`);
-    const pdf = await page.pdf({
-      path: pdfPath,
-      format: 'A4',
-    });
-    await browser.close();
-    return { path: pdfPath, length: pdf.length };
-  } catchh (error) {
-    console.error(error);
-    return error
-  }
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto(`${frontendUrl}/renderpdf/${id}/${accessToken}`, {
+    waitUntil: 'networkidle2',
+  });
+  await page.setViewport({ width: 1680, height: 1050 });
+  if (!fs.existsSync('./pdfs')) fs.mkdirSync('./pdfs');
+  const pdfPath = path.join(__dirname, 'pdfs', `travellerScreeningReport-${id}.pdf`);
+  const pdf = await page.pdf({
+    path: pdfPath,
+    format: 'A4',
+  });
+  await browser.close();
+  return { path: pdfPath, length: pdf.length };
 };
 
 module.exports = createPdf;
