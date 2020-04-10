1433709d09de1d80ec1706cde2d97c2497c53f34
Michel Alexander <michel@freshworks.io>
2020-04-09 17:37:47 -0700

    phone number to href=tel

--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Box, Typography, Card, CardContent, Button } from '@material-ui/core';
-
+const aColor = {color: '#002C71'}
 const Contact = function({ classes }) {
   return (
     <Card>
@@ -20,12 +20,12 @@ const Contact = function({ classes }) {
             <b>International</b>
           </p>
           <p>
-            <Button className={classes.contactButton} variant="outlined" color="primary">1-604-412-0957</Button>
+            <Button className={classes.contactButton} variant="outlined" color="primary"><a style={aColor} href="tel:+16044120957">1-604-412-0957</a></Button>
           </p>
           <p><b>Within Canada</b></p>
-          <p><Button className={classes.contactButton} color="primary" variant="outlined">Text 1-604-630-0300</Button></p>
-          <p><Button className={classes.contactButton} color="primary" variant="outlined">Call 1-888-COVID19</Button></p>
-          <p><a style={{color: '#002C71'}} href="tel:1-888-268-4319">(1-888-268-4319)</a></p>
+          <p><Button className={classes.contactButton} color="primary" variant="outlined">Text<a style={aColor} href="tel:+16046300300">1-604-630-0300</a></Button></p>
+          <p><Button className={classes.contactButton} color="primary" variant="outlined">Call<a style={aColor} href="tel:+1888COVID19">1-888-COVID19</a></Button></p>
+          <p><a style={aColor} href="tel:+18882684319">(1-888-268-4319)</a></p>
           <p>Standard message and data rates may apply.</p>
           <hr />
           <p><b>Telephone for the Deaf</b></p>
@@ -42,7 +42,7 @@ const Contact = function({ classes }) {
             <li>Servicios de traducción</li>
           </ul>
           <p><b>Service is available 7:30 am to 8 pm Pacific Time</b></p>
-          <div><Button className={classes.contactButton} color="primary" variant="outlined">Call 1-888-268-4319</Button></div>
+          <div><Button className={classes.contactButton} color="primary" variant="outlined">Call <a style={aColor} href="tel:+18882684319">1-888-268-4319</a></Button></div>
         </Box>
       </CardContent>
     </Card>
