2e04355053ca6f06f688f62232d0dd1f9cfa6980
Ashton Meuser <ashtonmeuser@gmail.com>
2020-04-10 21:02:31 -0700

    Linting

--- server/require-https.js
+++ server/require-https.js
@@ -1,9 +1,9 @@
-var requireHTTPS = function(req, res, next) {
+const requireHttps = (req, res, next) => {
   // The 'x-forwarded-proto' check
-  if (!req.secure && req.get('x-forwarded-proto') !== 'https' && process.env.NODE_ENV !== "development") {
-    return res.redirect('https://' + req.get('host') + req.url);
+  if (!req.secure && req.get('x-forwarded-proto') !== 'https' && process.env.NODE_ENV !== 'development') {
+    return res.redirect(`https://${req.get('host')}${req.url}`);
   }
-  next();
-}
+  return next();
+};
 
-module.exports = requireHTTPS
+module.exports = requireHttps;
