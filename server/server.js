7fc1d8479977a600c86e2fae5813ef5b7bfcbe08
Hitankar Ray <hitankar@freshworks.io>
2020-04-10 18:56:02 -0700

    express https

--- server/server.js
+++ server/server.js
@@ -9,6 +9,16 @@ const createPdf = require('./pdf.js');
 const apiBaseUrl = '/api/v1';
 const port = 80;
 const app = express();
+
+var requireHTTPS = function(req, res, next) {
+  // The 'x-forwarded-proto' check
+  if (!req.secure && req.get('x-forwarded-proto') !== 'https' && process.env.NODE_ENV !== "development") {
+    return res.redirect('https://' + req.get('host') + req.url);
+  }
+  next();
+}
+
+app.use(requireHTTPS);
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '../client/build')));
 
