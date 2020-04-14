31d3293d378ab90e6c934c4dc8d9e0b1f5f50633
Ashton Meuser <ashtonmeuser@gmail.com>
2020-04-14 11:43:50 -0700

    Don't double write to SBC table

--- server/utils/ServiceBC.js
+++ server/utils/ServiceBC.js
@@ -1,14 +1,10 @@
 const axios = require('axios');
 const qs = require('querystring');
-const { randomBytes } = require('crypto');
-const { db, serviceBCTable } = require('../database.js');
+const NodeCache = require('node-cache');
 
-
-const url = 'https://sso.pathfinder.gov.bc.ca/auth/realms/vtkayq4c/protocol/openid-connect/token';
-
-let submitURL = 'https://test-serviceflow.pathfinder.gov.bc.ca/camunda/engine-rest/process-definition/key/covid_travel_plan_gateway/start';
-
-if (process.env.DB_SUFFIX === 'production') { submitURL = 'https://serviceflow.pathfinder.gov.bc.ca/camunda/engine-rest/process-definition/key/covid_travel_plan_gateway/start'; }
+const tokenUrl = 'https://sso.pathfinder.gov.bc.ca/auth/realms/vtkayq4c/protocol/openid-connect/token';
+const submitURL = `https://${process.env.DB_SUFFIX === 'production' ? '' : 'test-'}serviceflow.pathfinder.gov.bc.ca/camunda/engine-rest/process-definition/key/covid_travel_plan_gateway/start`;
+const appCache = new NodeCache();
 
 // TODO Move to env vars
 const auth = {
@@ -20,17 +16,20 @@ const auth = {
 };
 
 const getToken = async () => {
-  const { data } = await axios.post(url, qs.stringify(auth), {
+  if (appCache.get('ServiceBCToken')) return appCache.get('ServiceBCToken');
+  const { data } = await axios.post(tokenUrl, qs.stringify(auth), {
     headers: {
       'Content-Type': 'application/x-www-form-urlencoded',
       'Accept-Encoding': 'application/json',
     },
   });
-  return data;
+  appCache.set('ServiceBCToken', data.access_token, data.expires_in - 10);
+  return data.access_token;
 };
 
-const postItem = async (item, token) => {
+const postServiceItem = async (item) => {
   try {
+    const token = await getToken();
     const response = await axios.post(submitURL, {
       variables: {
         rawvariables: {
@@ -45,27 +44,17 @@ const postItem = async (item, token) => {
         Authorization: `Bearer ${token}`,
       },
     });
-    return response.data;
-  } catch (error) {
-    const id = randomBytes(10).toString('hex').toUpperCase();
-    const errorData = {
-      TableName: serviceBCTable,
-      Item: {
-        id,
-        confirmationId: item.id,
-        status: 'fail',
-        errorDetails: error.toJSON(),
-        createdAt: new Date().toISOString(),
-      },
-      ConditionExpression: 'attribute_not_exists(id)',
+    return {
+      status: 'success',
+      serviceBCId: response.data.id,
     };
-    await db.put(errorData).promise();
-    return false;
+  } catch (error) {
+    return {
+      status: 'fail',
+      errorDetails: JSON.stringify(error),
+    };
   }
 };
 
 // start function call
-module.exports = {
-  getToken,
-  postItem,
-};
+module.exports = postServiceItem;
