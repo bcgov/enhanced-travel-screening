0b7b3e612aad2fedbe24b352b484250a618a2c26
Mauricio Jourdan <mauricio@freshworks.io>
2020-05-28 16:21:36 -0700

    removed lint error

--- server/cron-job.js
+++ server/cron-job.js
@@ -5,12 +5,12 @@ const initCronJobs = () => {
   const timezone = 'America/Los_Angeles';
   const cronTime = '0 59 23 * * *'; // 23:59:00
 
-  CronJob(cronTime, () => {
+  const phacToSbc = new CronJob(cronTime, () => {
     const currentDate = new Date().toISOString();
-    logger.info('phac-sbc cron job executed', currentDate);
-
-    // TODO run phac -> sbc logic
-  }, null, true, timezone);
+    logger.info('phac to sbc cron job executed', currentDate);
+    // TODO run phac to sbc logic
+  }, null, false, timezone);
+  phacToSbc.start();
 };
 
 module.exports = initCronJobs;
