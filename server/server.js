e01c8b1777ae0aac0c59f2f0289d37aaac800c25
Mauricio Jourdan <mauricio@freshworks.io>
2020-06-02 12:15:21 -0700

    fixed conflict

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ const { randomBytes } = require('crypto');
 const { passport } = require('./auth.js');
 const requireHttps = require('./require-https.js');
 const { postServiceItem } = require('./utils/service-bc.js');
+const deriveTravellerKey = require('./utils/derive-traveller-key.js');
 const {
   validate, FormSchema, DeterminationSchema, PhacSchema,
 } = require('./validation.js');
