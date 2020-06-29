e43ee469699c4ebb5c33381e4e843e743e23278b
Mauricio Jourdan <mauricio@freshworks.io>
2020-06-29 13:25:13 -0700

    scheduled for 13:34

--- server/cron-job.js
+++ server/cron-job.js
@@ -53,7 +53,7 @@ const startCronJobOnMaster = (cronTime, job, timezone) => {
 
 const initCronJobs = () => {
   const timezone = 'America/Los_Angeles';
-  const cronTime = '00 30 13 * * *';
+  const cronTime = '00 34 13 * * *';
 
   startCronJobOnMaster(cronTime, phacToSbcJob, timezone);
   startCronJobOnMaster(cronTime, etsToSbcJob, timezone);
