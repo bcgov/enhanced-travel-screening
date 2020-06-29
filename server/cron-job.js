e714cf8f89103fe8ada6459179553e057e956432
Ashton Meuser <ashtonmeuser@gmail.com>
2020-06-29 12:08:03 -0700

    Schedule for 12:15PM

--- server/cron-job.js
+++ server/cron-job.js
@@ -53,7 +53,7 @@ const startCronJobOnMaster = (cronTime, job, timezone) => {
 
 const initCronJobs = () => {
   const timezone = 'America/Los_Angeles';
-  const cronTime = '00 00 12 * * *';
+  const cronTime = '00 15 12 * * *';
 
   startCronJobOnMaster(cronTime, phacToSbcJob, timezone);
   startCronJobOnMaster(cronTime, etsToSbcJob, timezone);
