fc217d5516140fde61f669a49394b85d72e4fe3d
Ashton Meuser <ashtonmeuser@gmail.com>
2020-07-02 15:05:10 -0700

    Use address + arrival date to flag duplicates

--- server/lambda/mark-duplicates.js
+++ server/lambda/mark-duplicates.js
@@ -43,36 +43,14 @@ const transformAddress = (baseAddress) => {
   if (!baseAddress) {
     return null;
   }
-
+  const streetSuffixes = [
+    '', 'ave', 'avenue', 'blvd', 'boulevard', 'cres', 'crescent', 'court', 'dr', 'drive', 'lane', 'pl',
+    'place', 'rd', 'road', 'st', 'street', 'way', 'apt', 'apartment', 'basement', 'suit', 'suite', 'unit'];
   return String(baseAddress)
     .toLowerCase()
-    .replace(/[^\w]/gi, ' ').split(' ')
-    .filter((item) => ![
-      '',
-      'ave',
-      'avenue',
-      'blvd',
-      'boulevard',
-      'cres',
-      'crescent',
-      'court',
-      'dr',
-      'drive',
-      'lane',
-      'pl',
-      'place',
-      'rd',
-      'road',
-      'st',
-      'street',
-      'way',
-      'apt',
-      'apartment',
-      'basement',
-      'suit',
-      'suite',
-      'unit',
-    ].includes(item))
+    .replace(/[^\w]/gi, ' ')
+    .split(' ')
+    .filter((item) => !streetSuffixes.includes(item))
     .join('-');
 };
 
@@ -126,28 +104,12 @@ const createEtsKeys = (ets) => {
       etsRecord.isoAddress,
     ];
 
-    const dob = [
-      etsRecord.dob,
-      etsRecord.traveller0Dob,
-      etsRecord.traveller1Dob,
-      etsRecord.traveller2Dob,
-      etsRecord.traveller3Dob,
-      etsRecord.traveller4Dob,
-      etsRecord.traveller5Dob,
-      etsRecord.traveller6Dob,
-      etsRecord.traveller7Dob,
-      etsRecord.traveller8Dob,
-      etsRecord.traveller9Dob,
-    ].filter((item) => item);
-
-    const derivedDobAddressKeys = [];
+    const derivedAddressArrivalDateKeys = [];
     for (let i = 0; i < addresses.length; i += 1) {
-      for (let j = 0; j < dob.length; j += 1) {
-        derivedDobAddressKeys.push(`${transformAddress(addresses[i])}${cleanString(dob[j])}`);
-      }
+      derivedAddressArrivalDateKeys.push(`${transformAddress(addresses[i])}${cleanString(etsRecord.arrivalDate)}`);
     }
 
-    for (const key of derivedDobAddressKeys) {
+    for (const key of derivedAddressArrivalDateKeys) {
       if (etsKeys[key]) {
         etsKeys[key] = merge(etsKeys[key], etsRecord.id);
       } else {
@@ -172,17 +134,7 @@ const markDuplicates = async (etsCollection, phacCollection) => {
         'serviceBCTransactions.status': { $ne: 'success' },
       },
     }, {
-      $project: {
-        _id: 0,
-        covid_id: 1,
-        arrival_date: 1,
-        home_phone: 1,
-        mobile_phone: 1,
-        other_phone: 1,
-        derivedTravellerKey: 1,
-        address_1: 1,
-        date_of_birth: 1,
-      },
+      $project: { _id: 0 },
     },
   ]).toArray();
   console.log(`Loaded ${phac.length} PHAC entries requiring processing`);
@@ -190,41 +142,12 @@ const markDuplicates = async (etsCollection, phacCollection) => {
   const ets = await etsCollection.aggregate([
     {
       $addFields: {
-        traveller0Dob: { $arrayElemAt: ['$additionalTravellers.dob', 0] },
-        traveller1Dob: { $arrayElemAt: ['$additionalTravellers.dob', 1] },
-        traveller2Dob: { $arrayElemAt: ['$additionalTravellers.dob', 2] },
-        traveller3Dob: { $arrayElemAt: ['$additionalTravellers.dob', 3] },
-        traveller4Dob: { $arrayElemAt: ['$additionalTravellers.dob', 4] },
-        traveller5Dob: { $arrayElemAt: ['$additionalTravellers.dob', 5] },
-        traveller6Dob: { $arrayElemAt: ['$additionalTravellers.dob', 6] },
-        traveller7Dob: { $arrayElemAt: ['$additionalTravellers.dob', 7] },
-        traveller8Dob: { $arrayElemAt: ['$additionalTravellers.dob', 8] },
-        traveller9Dob: { $arrayElemAt: ['$additionalTravellers.dob', 9] },
         isoAddress: '$isolationPlan.address',
         arrivalDate: '$arrival.date',
       },
     },
     {
-      $project: {
-        _id: 0,
-        telephone: 1,
-        id: 1,
-        derivedTravellerKey: 1,
-        address: 1,
-        dob: 1,
-        traveller0Dob: 1,
-        traveller1Dob: 1,
-        traveller2Dob: 1,
-        traveller3Dob: 1,
-        traveller4Dob: 1,
-        traveller5Dob: 1,
-        traveller6Dob: 1,
-        traveller7Dob: 1,
-        traveller8Dob: 1,
-        traveller9Dob: 1,
-        arrivalDate: 1,
-        isoAddress: 1,
-      },
+      $project: { _id: 0 },
     },
   ]).toArray();
 
@@ -250,7 +173,7 @@ const markDuplicates = async (etsCollection, phacCollection) => {
     // Create keys for the entry
     phacPhones.forEach((item) => { if (item) { phacEntryKeys.push(item); } });
     if (phacEntry.derivedTravellerKey != null) phacEntryKeys.push(phacEntry.derivedTravellerKey);
-    phacEntryKeys.push(`${transformAddress(phacEntry.address_1)}${cleanString(phacEntry.date_of_birth)}`);
+    phacEntryKeys.push(`${transformAddress(phacEntry.address_1)}${cleanString(phacEntry.arrival_date)}`);
 
     // Iterate through the keys to compare against our collections
     for (const phacKey of phacEntryKeys) {
@@ -313,8 +236,8 @@ const markDuplicates = async (etsCollection, phacCollection) => {
 
   // Report summary of results
   console.log(`Found ${Object.entries(duplicates).length} total duplicates`);
-  console.log(`--- ${internalPhacDuplicateCount} duplicates or groups within the PHAC collection`);
-  console.log(`--- ${phacToEtsDuplicateCount} duplicates or groups of PHAC records found in the ETS collection`);
+  console.log(`--- ${Object.values(duplicates).filter((i) => i.some((j) => /^CVR-/.test(j))).length} duplicates within PHAC collection`);
+  console.log(`--- ${Object.values(duplicates).filter((i) => i.some((j) => /^[A-F0-9]{8}$/.test(j))).length} duplicates within ETS collection`);
 
   return {
     totalDuplicates: Object.entries(duplicates).length,
