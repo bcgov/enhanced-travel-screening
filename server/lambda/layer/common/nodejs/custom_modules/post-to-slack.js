05c6bff0cc98382890df2cdcf8b74b87641929ae
Ashton Meuser <ashtonmeuser@gmail.com>
2020-07-27 14:24:38 -0700

    Remove accessory button if not in Lambda environment

--- server/lambda/layer/common/nodejs/custom_modules/post-to-slack.js
+++ server/lambda/layer/common/nodejs/custom_modules/post-to-slack.js
@@ -6,34 +6,40 @@ const generateStreamLink = () => {
     .replace(/\[/g, '$255B')
     .replace(/\]/g, '$255D')
     .replace(/\//g, '$252F');
+  if (['AWS_LAMBDA_LOG_GROUP_NAME', 'AWS_LAMBDA_LOG_STREAM_NAME', 'AWS_REGION']
+    .map((x) => process.env[x])
+    .some((x) => typeof x === 'undefined')) return null;
   const logGroup = encode(process.env.AWS_LAMBDA_LOG_GROUP_NAME);
   const logStream = encode(process.env.AWS_LAMBDA_LOG_STREAM_NAME);
   return `https://${process.env.AWS_REGION}.console.aws.amazon.com/cloudwatch/home?region=${process.env.AWS_REGION}#logsV2:log-groups/log-group/${logGroup}/log-events/${logStream}`;
 };
 
 const formatTime = (start) => {
+  if (!start) return 'N/A';
   const elapsed = (new Date().getTime() - start) / 1000;
   return `${Math.floor(elapsed / 60)}m${Math.round(elapsed % 60)}s`;
 };
 
-const formatMessage = (title, time, message) => `
-{ "blocks": [
-  {
-    "type": "section",
-    "text": {
-      "type": "mrkdwn",
-      "text": "Lambda Job: *${title}*\nFunction Name: ${process.env.AWS_LAMBDA_FUNCTION_NAME}:${process.env.AWS_LAMBDA_FUNCTION_VERSION}\nElapsed Time: ${time}\n\nFull output included below."
+const formatMessage = (title, time, message) => {
+  const link = generateStreamLink();
+  return `{ "blocks": [
+    {
+      "type": "section",
+      "text": {
+        "type": "mrkdwn",
+        "text": "Lambda Job: *${title}*\nFunction Name: ${process.env.AWS_LAMBDA_FUNCTION_NAME}:${process.env.AWS_LAMBDA_FUNCTION_VERSION}\nElapsed Time: ${time}\n\nFull output included below."
+      },
+      ${link == null ? '' : `"accessory": {
+        "type": "button",
+        "text": { "type": "plain_text", "text": "View CW Logs" },
+        "action_id": "view_logs",
+        "url": "${generateStreamLink()}"
+      }`}
     },
-    "accessory": {
-      "type": "button",
-      "text": { "type": "plain_text", "text": "View CW Logs" },
-      "action_id": "view_logs",
-      "url": "${generateStreamLink()}"
-    }
-  },
-  { "type": "divider" },
-  { "type": "section", "text": { "type": "plain_text", "text": "${message}" } }
-] }`;
+    { "type": "divider" },
+    { "type": "section", "text": { "type": "plain_text", "text": "${message}" } }
+  ] }`;
+};
 
 const postToSlack = async (title, start, ...messages) => {
   if (!/^https:\/\/hooks\.slack\.com/.test(process.env.SLACK_ENDPOINT)) throw Error('No valid Slack endpoint specified');
