1e2ec9619969ed8cd796b4c1c5005bd72ae65d80
Kendall Olsen-Maier <kendall.olsen-maier@aplaceformom.com>
2020-08-21 13:52:08 -0700

    Fixed revert.

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const helmet = require('helmet');
 const bodyParser = require('body-parser');
 const path = require('path');
 const { randomBytes } = require('crypto');
