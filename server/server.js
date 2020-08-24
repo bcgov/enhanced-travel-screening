04a62e7edee2d421860d574e95e1cce114a3540f
Ashton Meuser <ashtonmeuser@gmail.com>
2020-08-24 14:17:02 -0700

    Use defaults from Helmet
    See https://github.com/helmetjs/helmet/blob/1b53850392105e080a9a0c149cda1b294a442ce6/middlewares/content-security-policy/index.ts#L20

--- server/server.js
+++ server/server.js
@@ -21,10 +21,17 @@ const app = express();
 app.use(helmet({
   contentSecurityPolicy: {
     directives: {
-      defaultSrc: ["'self'"],
-      scriptSrc: ["'self'", "'unsafe-inline'", 'https://*.apps.gov.bc.ca'],
-      objectSrc: ["'none'"],
-      upgradeInsecureRequests: [],
+      'default-src': ["'self'"],
+      'base-uri': ["'self'"],
+      'block-all-mixed-content': [],
+      'font-src': ["'self'", 'https:', 'data:'],
+      'frame-ancestors': ["'self'"],
+      'img-src': ["'self'", 'data:'],
+      'object-src': ["'none'"],
+      'script-src': ["'self'", "'unsafe-inline'", 'https://*.apps.gov.bc.ca'],
+      'script-src-attr': ["'none'"],
+      'style-src': ["'self'", 'https:', "'unsafe-inline'"],
+      'upgrade-insecure-requests': [],
     },
   },
 }));
