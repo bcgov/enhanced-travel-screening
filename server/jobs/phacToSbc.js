c039d3369d11fc0b62c7b7d6516791b6f229e35e
Ashton Meuser <ashtonmeuser@gmail.com>
2020-10-02 16:53:49 -0700

    Connect to DB before and after mark duplicates job

--- server/jobs/phacToSbc.js
+++ server/jobs/phacToSbc.js
@@ -8,15 +8,18 @@ const dbConnectionAndCollections = require('./common/db');
   const { tunnel, connection, collections } = await dbConnectionAndCollections(['ets-forms', 'ets-phac']);
   const [etsCollection, phacCollection] = collections;
   try {
+    const etsCount1 = await etsCollection.countDocuments({});
+    const phacCount1 = await phacCollection.countDocuments({});
+    console.log('Count 1', etsCount1, phacCount1);
     console.log('Is disabled:', process.env.DB_WRITE_SERVICE_DISABLED);
     const duplicates = await markDuplicates(etsCollection, phacCollection);
     console.log(duplicates);
     // const transactions = await sendPhacToSBC(phacCollection);
     // console.log(transactions);
     // await postToSlack('PHAC to Service BC (OCP)', start, duplicates, transactions);
-    const etsCount = await etsCollection.countDocuments({});
-    const phacCount = await phacCollection.countDocuments({});
-    console.log(etsCount, phacCount);
+    const etsCount2 = await etsCollection.countDocuments({});
+    const phacCount2 = await phacCollection.countDocuments({});
+    console.log('Count 2', etsCount2, phacCount2);
   } catch (error) {
     console.error(`Failed to mark duplicates or post to SBC ${error}`);
   } finally {
