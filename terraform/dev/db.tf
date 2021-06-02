f6a974262ea619a5ac6d5945fa658ca2430cbff8
v-y-a-s <10957095+v-y-a-s@users.noreply.github.com>
2021-06-02 23:30:28 +0530

    db

---
+++ terraform/dev/db.tf
@@ -0,0 +1,47 @@
+resource "aws_docdb_cluster" "db_cluster" {
+  cluster_identifier              = local.namespace
+  engine                          = "docdb"
+  master_username                 = "admin"
+  master_password                 = "etsoperations123"
+  backup_retention_period         = 5
+  preferred_backup_window         = "10:00-12:00"
+  skip_final_snapshot             = false
+  final_snapshot_identifier       = "${var.target_env}finalbackup"
+  apply_immediately               = false
+  db_cluster_parameter_group_name = aws_docdb_cluster_parameter_group.db_param_group.name
+  deletion_protection             = false
+  enabled_cloudwatch_logs_exports = ["audit", "profiler"]
+
+  db_subnet_group_name = aws_docdb_subnet_group.db_subnet_group.id
+  # TODO: vpc_security_group_ids = [local.data_security_group_name]
+  # TODO: kms_key_id = 
+  # TODO: storage_encrypted = 
+}
+
+
+resource "aws_docdb_subnet_group" "db_subnet_group" {
+  name       = "${var.target_env}-db-subnet-grp"
+  subnet_ids = module.network.aws_subnet_ids.data.ids
+}
+
+resource "aws_docdb_cluster_instance" "cluster_instances" {
+  count              = 2
+  identifier         = "${local.namespace}-docdb-instance-${count.index + 1}"
+  cluster_identifier = aws_docdb_cluster.db_cluster.id
+  apply_immediately  = true # TODO: SET TO FALSE AFTER DEV IS UP 
+  instance_class     = "db.t3.medium"
+  engine             = "docdb"
+}
+
+
+resource "aws_docdb_cluster_parameter_group" "db_param_group" {
+  family      = "docdb3.6"
+  name        = "${var.target_env}-docdb-param-group"
+  description = "docdb cluster parameter group"
+
+  parameter {
+    name         = "tls"
+    value        = "enabled"
+    apply_method = "pending-reboot"
+  }
+}
---
+++ terraform/dev/vpc.tf
@@ -0,0 +1,4 @@
+module "network" {
+  source      = "git::https://github.com/BCDevOps/terraform-octk-aws-sea-network-info.git//?ref=master"
+  environment = var.target_env
+}
