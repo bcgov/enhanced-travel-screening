82988c2f7c42530664088b56114ab331a09e5156
v-y-a-s <10957095+v-y-a-s@users.noreply.github.com>
2021-06-16 21:17:15 +0530

    migration help updats

--- scripts/ssm.sh
+++ scripts/ssm.sh
@@ -9,13 +9,3 @@ export AWS_ACCESS_KEY_ID=`echo "$CREDS" | jq -r '.Credentials.AccessKeyId'`
 export AWS_SECRET_ACCESS_KEY=`echo "$CREDS" | jq -r '.Credentials.SecretAccessKey'`
 export AWS_SESSION_TOKEN=`echo "$CREDS" | jq -r '.Credentials.SessionToken'`
 export AWS_DEFAULT_REGION="ca-central-1"
-
-mongodump --ssl --host ets-dev.cluster-cyk1pye3ihk7.ca-central-1.docdb.amazonaws.com:27017 --sslCAFile rds-combined-ca-bundle.pem --username etsmasterdev --password <password> --db ets-dev --out dump
-scp -r -i ~/.ssh/ets_bastion_host.pem ec2-user@ec2-3-96-194-192.ca-central-1.compute.amazonaws.com:/home/ec2-user/dump ./
-
-
-# local
-mongorestore --tls --tlsCAFile rds-combined-ca-bundle.pem --host klwrig-dev.cluster-cgcqnzo2vul6.ca-central-1.docdb.amazonaws.com:27017 --username root --password LrCQxCrtyHEEk6x2 --db ets-dev --drop ./dump/ets-dev/
-
-# dev
-mongorestore --ssl --host klwrig-dev.cluster-cgcqnzo2vul6.ca-central-1.docdb.amazonaws.com:27017 --username root --password <password> --sslCAFile rds-combined-ca-bundle.pem --db ets-dev --drop ./dump/ets-dev/
