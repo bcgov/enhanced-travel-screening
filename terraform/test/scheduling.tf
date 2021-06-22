0194461eca0fc19c0b9693f4dd12a45f4616cb8f
v-y-a-s <10957095+v-y-a-s@users.noreply.github.com>
2021-06-23 02:58:08 +0530

    replicate for test env

---
+++ terraform/test/scheduling.tf
@@ -0,0 +1,35 @@
+resource "aws_cloudwatch_event_rule" "every_day_morning" {
+  name                = "every-day-morning"
+  description         = "Fires every day at 8:45"
+  schedule_expression = "cron(45 8 * * ? *)"
+}
+
+resource "aws_cloudwatch_event_target" "call_phacToSbc_every_day_morning" {
+  rule      = aws_cloudwatch_event_rule.every_day_morning.name
+  target_id = "lambda"
+  arn       = aws_lambda_function.phacToSbc.arn
+}
+
+resource "aws_cloudwatch_event_target" "call_etsToSbc_every_day_morning" {
+  rule      = aws_cloudwatch_event_rule.every_day_morning.name
+  target_id = "lambda"
+  arn       = aws_lambda_function.etsToSbc.arn
+}
+
+resource "aws_cloudwatch_event_rule" "every_day_midnight" {
+  name                = "every-day-midnight"
+  description         = "Fires every day at midnight"
+  schedule_expression = "cron(0 0 * * ? *)"
+}
+
+resource "aws_cloudwatch_event_target" "call_phacToSbc_every_day_midnight" {
+  rule      = aws_cloudwatch_event_rule.every_day_midnight.name
+  target_id = "lambda"
+  arn       = aws_lambda_function.phacToSbc.arn
+}
+
+resource "aws_cloudwatch_event_target" "call_etsToSbc_every_day_midnight" {
+  rule      = aws_cloudwatch_event_rule.every_day_midnight.name
+  target_id = "lambda"
+  arn       = aws_lambda_function.etsToSbc.arn
+}
---
+++ terraform/test/variables.tf
@@ -0,0 +1,28 @@
+variable "target_env" {}
+variable "project_code" {}
+variable "git_version" {}
+variable "project_name" {}
+
+variable "target_aws_account_id" {}
+data "aws_caller_identity" "current" {}
+
+
+variable "api_zip" {}
+variable "ets_to_sbc_zip" {}
+variable "phac_to_sbc_zip" {}
+variable "azs" {
+  default = ["ca-central-1a", "ca-central-1b"]
+}
+
+variable "region" {
+  default = "ca-central-1"
+}
+
+locals {
+  namespace  = var.project_code
+  account_id = data.aws_caller_identity.current.account_id
+  app_name   = "${var.project_name}-app-${var.target_env}"
+  api_name = "${var.project_name}-server-${var.target_env}"
+  ets_to_sbc = "${var.project_name}-ets-sbc-${var.target_env}"
+  phac_to_sbc = "${var.project_name}-phac-sbc-${var.target_env}"
+}
