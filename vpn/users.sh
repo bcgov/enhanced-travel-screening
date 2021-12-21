bc20f6b552a99125852b131d9f7849c471b9c33e
v-y-a-s <10957095+v-y-a-s@users.noreply.github.com>
2021-12-20 18:38:52 -0800

    update generation script + docs

--- vpn/users.sh
+++ vpn/users.sh
@@ -3,21 +3,26 @@
 # This script generates certificate for client vpn
 # AWS Client VPN Pricing: https://aws.amazon.com/vpn/pricing/
 
-REGION=${1?"Region Parameter Missing !"}
-ENV=${2?"Environment Parameter Missing !"}
-DOMAIN=${3?"VPN Domain Name Missing !"}
-NAME=${4?"User Name Missing !"}
-ENDPOINT=${5?"Full Endpoint - cvpn-endpoint-xxxxxxxxx !"}
+# Run as follows:
 
+# export LZ2 cli credentials to terminal 
+# ./vpn/users.sh dev david $(aws ec2 describe-client-vpn-endpoints | jq -r '.ClientVpnEndpoints[0].ClientVpnEndpointId')
+
+ENV=${1?"Environment Parameter Missing !"}
+NAME=${2?"User Name Missing !"}
+ENDPOINT=${3?"Full Endpoint - cvpn-endpoint-xxxxxxxxx !"}
+
+REGION="ca-central-1"
+DOMAIN="${ENV}-ets-vpn.freshworks.club"
 REPO_LOCATION=$(git rev-parse --show-toplevel)
 
-export EASYRSA_BATCH=1
-
-client="$4"
-cert_domain="$3"
+client="$NAME"
+cert_domain="$DOMAIN"
 client_full="${client}.${cert_domain}"
 outfile="/tmp/${client}-${ENV}-cvpn-endpoint.ovpn"
 vpn_endpoint_id="$ENDPOINT"
+
+export EASYRSA_BATCH=1 # https://easy-rsa.readthedocs.io/en/latest/advanced/#environmental-variables-reference
 
 tput setaf 2; echo ">> clone repo ..."; tput setaf 9
 cd `mktemp -d`
@@ -51,7 +56,7 @@ tput setaf 2; echo ">> generate vpn client config file ..."; tput setaf 9
 
 # route only VPC CIDR through VPN
 echo "route-nopull"                 >> $outfile
-echo "route 10.12.0.0 255.255.0.0"  >> $outfile
+echo "route 10.0.0.0 255.0.0.0"  >> $outfile
 
 aws ec2 export-client-vpn-client-configuration \
     --client-vpn-endpoint-id $vpn_endpoint_id \
@@ -69,4 +74,5 @@ echo "</key>"                      >> $outfile
 tput setaf 2; echo ">> $outfile ..."; tput setaf 9
 ls -alh $outfile
 
-mv $outfile ${REPO_LOCATION}/
+mkdir -p ${REPO_LOCATION}/vpn/gen/${ENV}
+mv $outfile ${REPO_LOCATION}/vpn/gen/${ENV}/
