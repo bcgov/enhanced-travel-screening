62539c89b0efe1c5586b5d3514685c86b42d0771
v-y-a-s <10957095+v-y-a-s@users.noreply.github.com>
2021-12-22 20:53:42 -0800

    api alarms

---
+++ terraform/api_alarms.tf
@@ -0,0 +1,56 @@
+
+resource "aws_cloudwatch_log_metric_filter" "api_server_lambda_errors" {
+  name           = "ets_api_server_lambda_errors_filter"
+  pattern        = "?ERROR ?WARN"
+  log_group_name = "/aws/lambda/ets-server-dev"
+
+  metric_transformation {
+    name      = "lambda_unexpected_errors"
+    namespace = "lambda_alarms"
+    value     = "1"
+  }
+}
+
+resource "aws_cloudwatch_metric_alarm" "api_server_lambda_errors" {
+  alarm_name          = "api_server_lambda_errors"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = "1"
+  metric_name         = "lambda_unexpected_errors"
+  namespace           = "lambda_alarms"
+  period              = "30"
+  threshold           = "1"
+  statistic           = "Average"
+  alarm_description   = "This metric monitors server lambda alerts"
+  alarm_actions       = [aws_sns_topic.aws_alerts.arn]
+  ok_actions          = [aws_sns_topic.aws_alerts.arn]
+
+  insufficient_data_actions = []
+}
+
+resource "aws_cloudwatch_log_metric_filter" "api_server_levels" {
+  name           = "ets_api_server_levels_filter"
+  pattern        = "{ $.level = \"error\" }"
+  log_group_name = "/aws/lambda/ets-server-dev"
+
+  metric_transformation {
+    name      = "EventCount"
+    namespace = "lambda_alarms"
+    value     = "1"
+  }
+}
+
+resource "aws_cloudwatch_metric_alarm" "api_server_levels" {
+  alarm_name          = "api_server_levels"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = "1"
+  metric_name         = "EventCount"
+  namespace           = "lambda_alarms"
+  period              = "30"
+  threshold           = "1"
+  statistic           = "Average"
+  alarm_description   = "This metric monitors server lambda alerts"
+  alarm_actions       = [aws_sns_topic.aws_alerts.arn]
+  ok_actions          = [aws_sns_topic.aws_alerts.arn]
+
+  insufficient_data_actions = []
+}
--- terraform/notification.tf
+++ terraform/notification.tf
@@ -1,10 +1,3 @@
-module "notify_slack" {
-  source  = "terraform-aws-modules/notify-slack/aws"
-  version = "~> 4.0"
-
-  sns_topic_name = "${var.target_env}_slack_alerts"
-
-  slack_webhook_url = data.aws_ssm_parameter.slack_endpoint.value
-  slack_channel     = "health-pod-covid-response"
-  slack_username    = "aws-reporter"
+resource "aws_sns_topic" "aws_alerts" {
+  name = "${var.target_env}-aws-alerts"
 }
