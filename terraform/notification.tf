81bab1213dce164b6a9164483ddc279ce4a98b31
v-y-a-s <10957095+v-y-a-s@users.noreply.github.com>
2021-12-22 16:20:36 -0800

    use ssm

--- terraform/notification.tf
+++ terraform/notification.tf
@@ -4,7 +4,7 @@ module "notify_slack" {
 
   sns_topic_name = "${var.target_env}_slack_alerts"
 
-  slack_webhook_url = "https://hooks.slack.com/services/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"
+  slack_webhook_url = data.aws_ssm_parameter.slack_endpoint.value
   slack_channel     = "health-pod-covid-response"
   slack_username    = "aws-reporter"
 }
