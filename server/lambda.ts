936757670e58b4d90861ba7f316cd2a398097424
Hitankar Ray <hitankar@freshworks.io>
2022-01-06 13:43:46 -0800

    use logger instead of console

--- server/lambda.ts
+++ server/lambda.ts
@@ -2,6 +2,7 @@ import awsServerlessExpress from 'aws-serverless-express';
 import awsServerlessExpressMiddleware from 'aws-serverless-express/middleware';
 import app from './server';
 import { dbClient } from './db';
+import logger from 'logger';
 
 app.use(awsServerlessExpressMiddleware.eventContext());
 const wrappedApp = awsServerlessExpress.createServer(app);
@@ -9,10 +10,10 @@ const wrappedApp = awsServerlessExpress.createServer(app);
 (async () => {
   try {
     const connection = await dbClient.connect();
-    console.log('connection success', connection);
+    logger.log('connection success', connection);
   } catch (err) {
-    console.log('Database Connection Failed');
-    console.log(err);
+    logger.error('Database Connection Failed');
+    logger.error(err);
   }
 })();
 
