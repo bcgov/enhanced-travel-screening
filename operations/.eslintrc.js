7f1907a3fabd05f9ba5f1bf4ffba1a8a797db350
Sunghwan Park <95253983+sunghwan-park-fw@users.noreply.github.com>
2022-01-11 13:18:43 -0800

    ETS-520 | migrate bitbucket scripts to github (#180)
    ETS-520 | migrate bitbucket scripts to github

---
+++ operations/.eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    node: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 13,
+    sourceType: 'module',
+  },
+  plugins: [
+    '@typescript-eslint',
+  ],
+  rules: {
+  },
+};
