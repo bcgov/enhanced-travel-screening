a060e5b0ea27028ae2fe22e4423d58c7689ac246
Sunghwan Park <sunghwan@freshworks.io>
2022-01-11 16:52:28 -0800

    ETS-531 | fix index error

--- server/src/utils/transform-validation-errors.ts
+++ server/src/utils/transform-validation-errors.ts
@@ -1,15 +1,15 @@
 import { PhacEntryError } from '../types';
 
 export const transformValidationErrors = (errors: string[]): Record<string, PhacEntryError> => {
-  return errors.reduce((errors, yupMessage) => {
-    const found = yupMessage.match(/^(?<id>.*):\[(?<index>\d+)\]\.(?<message>.*)$/);
+  return errors.reduce((errors, yupError) => {
+    const found = yupError.match(/^(?<id>.*):\[(?<index>\d+)\]\.(?<message>.*)$/);
     if (!found) return;
 
     const { index, id, message } = found.groups;
-    let error = errors.index;
+    let error = errors[index];
     if (!error) {
       error = { id, index, errors: [] };
-      errors.index = error;
+      errors[index] = error;
     }
     error.errors.push(message);
 
