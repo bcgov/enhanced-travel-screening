b186967b3105acc206d28ef2c93161e2873488d1
Vyas <67800715+vyasworks@users.noreply.github.com>
2022-01-11 18:10:38 -0800

    deprecate client (#183)

--- client/src/utils/date.js
+++
@@ -1,5 +0,0 @@
-import moment from 'moment';
-
-export const dateToString = (dateObj) => moment(dateObj).format('YYYY/MM/DD');
-
-export const stringToDate = (dateStr) => moment(dateStr, 'YYYY/MM/DD');
